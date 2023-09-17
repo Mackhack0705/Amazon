63976fc30f9e279c3f321addfcf3bc1ccb145dd4
Rajesh <malirajesh0705@gmail.com>
2023-09-17 14:38:40 +0530

    made some other changes

--- myapp/src/Components/ImgCard.jsx
+++ myapp/src/Components/ImgCard.jsx
@@ -4,27 +4,31 @@ import Card from 'react-bootstrap/Card';
 import '../style/cards.css';
 
 const ImgCard = (val) => {
-  
+
   return (
     <>
-    <div className="productsCard">
-    <div className='box'>
-    <Card className='card' style={{ width: '18rem',height: '38rem'}}>
-      <Card.Img className='img' variant="top" src={val.imgurl} />
-      <Card.Body className='cardbody'>
-        <Card.Title className='title'>{val.title}</Card.Title>
-        <Card.Text className='rate price'>{val.rate}⭐⭐⭐⭐⭐{val.count}</Card.Text>
-        <Card.Text className='price'>
-        <sup className='sup'>$ </sup>
-        {val.price}
-        </Card.Text>
-        <Card.Text className='txt price'>Ships to india</Card.Text>
-        <Button variant="primary" className='button' id='atc' onClick={val.onButtonClick1}>Add to Cart</Button>
-        <Button variant="primary" className='button' id='bn' onClick={val.onButtonClick2}>Buy Now</Button>
-      </Card.Body>
-    </Card>
-    </div>
-    </div>
+      <div className="productsCard">
+        <Card className='card'>
+          <div className="pro-box">
+            <div className="sub">
+              <Card.Img className='img' variant="top" src={val.imgurl} />
+            </div>
+            <div className="sub">
+              <Card.Body className='cardbody'>
+                <Card.Title className='title'>{val.title}</Card.Title>
+                <Card.Text className='rate price'>{val.rate}⭐⭐⭐⭐⭐{val.count}</Card.Text>
+                <Card.Text className='price'>
+                  <sup className='sup'>$ </sup>
+                  {val.price}
+                </Card.Text>
+                <Card.Text className='txt price'>Ships to india</Card.Text>
+                <Button variant="primary" className='button' id='atc' onClick={val.onButtonClick1}>Add to Cart</Button>
+                <Button variant="primary" className='button' id='bn' onClick={val.onButtonClick2}>Buy Now</Button>
+              </Card.Body>
+            </div>
+          </div>
+        </Card>
+      </div>
     </>
   )
 }
